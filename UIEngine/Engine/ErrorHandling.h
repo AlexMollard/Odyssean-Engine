517d7ffc4067dfd2080b6e8e2dce2a5eab4beb91
Alex Mollard <alex.mollard@mail.com>
2022-12-22 22:49:57 +1100

    Fixed scoping for ErrorHandling.h

--- UIEngine/Engine/ErrorHandling.h
+++ UIEngine/Engine/ErrorHandling.h
@@ -4,32 +4,38 @@
 
 #if defined(_MSC_VER) && (!__clang__)
 // BreakPoint on line and output in console just in case using the printf format
-#define S_ASSERT(inValue, message)                                                                                  \
-	if (!(inValue))                                                                                                 \
-	{                                                                                                               \
-		const std::source_location location = std::source_location::current();                                      \
-		std::clog << "ASSERT:\n"                                                                                    \
-				  << "file: " << location.file_name() << "(" << location.line() << ":" << location.column() << ") " \
-				  << location.function_name() << ": " << message << '\n';                                           \
-		__debugbreak();                                                                                             \
+#define S_ASSERT(inValue, message)                                                                                      \
+	{                                                                                                                   \
+		if (!(inValue))                                                                                                 \
+		{                                                                                                               \
+			const std::source_location location = std::source_location::current();                                      \
+			std::clog << "ASSERT:\n"                                                                                    \
+					  << "file: " << location.file_name() << "(" << location.line() << ":" << location.column() << ") " \
+					  << location.function_name() << ": " << message << '\n';                                           \
+			__debugbreak();                                                                                             \
+		}                                                                                                               \
 	}
 
 // Same as above just don't breakpoint
-#define S_WARN(inValue, message)                                                                                    \
-	if (!(inValue))                                                                                                 \
-	{                                                                                                               \
-		const std::source_location location = std::source_location::current();                                      \
-		std::clog << "WARN:\n"                                                                                      \
-				  << "file: " << location.file_name() << "(" << location.line() << ":" << location.column() << ") " \
-				  << location.function_name() << ": " << message << '\n';                                           \
+#define S_WARN(inValue, message)                                                                                        \
+	{                                                                                                                   \
+		if (!(inValue))                                                                                                 \
+		{                                                                                                               \
+			const std::source_location location = std::source_location::current();                                      \
+			std::clog << "WARN:\n"                                                                                      \
+					  << "file: " << location.file_name() << "(" << location.line() << ":" << location.column() << ") " \
+					  << location.function_name() << ": " << message << '\n';                                           \
+		}                                                                                                               \
 	}
 
 // Just a info output with file info
-#define S_INFO(message)                                                                                         \
-	const std::source_location location = std::source_location::current();                                      \
-	std::clog << "Info:\n"                                                                                      \
-			  << "file: " << location.file_name() << "(" << location.line() << ":" << location.column() << ") " \
-			  << location.function_name() << ": " << message << '\n';
+#define S_INFO(message)                                                                                             \
+	{                                                                                                               \
+		const std::source_location location = std::source_location::current();                                      \
+		std::clog << "Info:\n"                                                                                      \
+				  << "file: " << location.file_name() << "(" << location.line() << ":" << location.column() << ") " \
+				  << location.function_name() << ": " << message << '\n';                                           \
+	}
 #endif
 
 #if defined(__clang__)
